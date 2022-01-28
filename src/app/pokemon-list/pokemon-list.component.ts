33122fbf496d61820895fcc30197d3a2425a886d
Jesus <jesusitoo486@gmail.com>
2022-01-27 18:49:30 -0600

    Consumo de Api

---
+++ src/app/pokemon-list/pokemon-list.component.ts
@@ -0,0 +1,36 @@
+
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../service/data.service';
+
+@Component({
+  selector: 'app-pokemon-list',
+  templateUrl: './pokemon-list.component.html',
+  styleUrls: ['./pokemon-list.component.css']
+})
+export class PokemonListComponent implements OnInit {
+  pokemons : any[] =[];
+  //page =1 ;
+  //totalPokemons!: number;
+
+  constructor(private dataService: DataService) {}
+
+  ngOnInit(): void {
+    this.dataService.getPokemons()
+    .subscribe((response: any)  => {
+    //  this.totalPokemons= response.count;
+
+      response.results.forEach((result: { name: string; }) => {
+        this.dataService.getMoreData(result.name)
+        .subscribe((uniqResponse: any) => {
+          this.pokemons.push(uniqResponse);
+          console.log(this.pokemons);
+        });
+
+      });
+
+    });
+
+
+
+  }
+}
